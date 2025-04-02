5e116ca532fa8ae4b6df3362bd878b4e2c6a51c4
kamila <ksalimzh@gmail.com>
2025-04-02 22:51:21 +0500

    Initial Commit

---
+++ mapkitapp/mapkitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkitappApp.swift
+//  mapkitapp
+//
+//  Created by kamila on 02.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct mapkitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
